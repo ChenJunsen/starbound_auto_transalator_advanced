c12d0207bdb3b3f5f18712f73dfc67f703fc759b
陈俊森 <022927@sichuanair.com>
2024-10-15 15:53:31 +0800

    增加TRANSLATE_IF_ENG_EQL_CHS配置属性

--- src/exe.js
+++ src/exe.js
@@ -1,61 +1,61 @@
 require('module-alias/register')
 const L = require('L')
 const fs = require('fs')
-const {TRANSLATE_DELAY, TRANSLATE_COVER, TARGET_PATH} = require('@root/project.config')
-const {loopFile} = require('@src/module/file')
-const {fuTranslate, fuRegroup} = require('@src/module/format/fu')
+const { TRANSLATE_DELAY, TRANSLATE_COVER, TARGET_PATH, TRANSLATE_IF_ENG_EQL_CHS } = require('@root/project.config')
+const { loopFile } = require('@src/module/file')
+const { fuTranslate, fuRegroup } = require('@src/module/format/fu')
 
 const fileList = loopFile(TARGET_PATH)//待翻译的文件列表
-L.d(`待翻译文件数量:${Array.isArray(fileList) ? fileList.length + 1 : 0}`)
+L.d(`待翻译文件数量:${ Array.isArray(fileList) ? fileList.length + 1 : 0 }`)
 
 function doIt(index) {
-    let fName = fileList[index]
-    L.d(`当前处理文件第${index + 1}个文件:`)
-    L.i(`[${fName}]`)
-    fs.open(fName, 'r+', (err, fd) => {
+  let fName = fileList[index]
+  L.d(`当前处理文件第${ index + 1 }个文件:`)
+  L.i(`[${ fName }]`)
+  fs.open(fName, 'r+', (err, fd) => {
+    if (err) {
+      L.e(err)
+    } else {
+      L.i(`文件打开成功:${ fd } 准备读取:`)
+      fs.readFile(fName, 'utf-8', (err, data) => {
         if (err) {
-            L.e(err)
+          L.e('文件读取失败！')
         } else {
-            L.i(`文件打开成功:${fd} 准备读取:`)
-            fs.readFile(fName, 'utf-8', (err, data) => {
+          fuTranslate(data, res => {
+            L.d('翻译结果')
+            L.i(res === -1 ? '当前文件已经翻译完全，跳过该文件' : JSON.stringify(res, null, 2))
+            L.rainbow('------------------------------------------------------------------')
+            if (res === -1) {
+              if (++index < fileList.length) {
+                doIt(index)
+              } else {
+                L.i('***********全部文件翻译完成**********')
+              }
+            } else {
+              let srcObj = JSON.parse(data)
+              fuRegroup(srcObj, res, TRANSLATE_COVER, TRANSLATE_IF_ENG_EQL_CHS)
+              console.log('重组结果')
+              console.log(srcObj)
+              fs.writeFileSync(fName, JSON.stringify(srcObj, null, 2))
+              fs.close(fd, (err) => {
                 if (err) {
-                    L.e('文件读取失败！')
-                } else {
-                    fuTranslate(data, res => {
-                        L.d('翻译结果')
-                        L.i(res === -1 ? '当前文件已经翻译完全，跳过该文件' : JSON.stringify(res, null, 2))
-                        L.rainbow('------------------------------------------------------------------')
-                        if (res === -1) {
-                            if (++index < fileList.length) {
-                                doIt(index)
-                            } else {
-                                L.i('***********全部文件翻译完成**********')
-                            }
-                        } else {
-                            let srcObj = JSON.parse(data)
-                            fuRegroup(srcObj, res, TRANSLATE_COVER)
-                            console.log('重组结果')
-                            console.log(srcObj)
-                            fs.writeFileSync(fName, JSON.stringify(srcObj, null, 2))
-                            fs.close(fd, (err) => {
-                                if (err) {
-                                    L.e('关闭失败')
-                                    L.e(err)
-                                }
-                            })
-                            if (++index < fileList.length) {
-                                setTimeout(() => {
-                                    doIt(index)
-                                }, TRANSLATE_DELAY)
-                            } else {
-                                L.i('***********全部文件翻译完成**********')
-                            }
-                        }
-                    }, TRANSLATE_COVER)
+                  L.e('关闭失败')
+                  L.e(err)
                 }
-            })
+              })
+              if (++index < fileList.length) {
+                setTimeout(() => {
+                  doIt(index)
+                }, TRANSLATE_DELAY)
+              } else {
+                L.i('***********全部文件翻译完成**********')
+              }
+            }
+          }, TRANSLATE_COVER, TRANSLATE_IF_ENG_EQL_CHS)
         }
-    })
+      })
+    }
+  })
 }
 
 doIt(0)
--- src/module/format/fu.js
+++ src/module/format/fu.js
@@ -1,7 +1,7 @@
 require('module-alias/register')
 const L = require('L')
-const {isEmptyStr} = require('@src/module/util')
-const {translate} = require('@src/lib/baidu.api')
+const { isEmptyStr } = require('@src/module/util')
+const { translate } = require('@src/lib/baidu.api')
 require('colors')
 
 /**
@@ -10,19 +10,19 @@ require('colors')
  * @returns {boolean}
  */
 function fuCheck(str) {
-    let res = false
-    if (!isEmptyStr(str)) {
-        try {
-            let jsonObj = JSON.parse(str)
-            res = Array.isArray(jsonObj) && jsonObj.length > 0
-        } catch (e) {
-            L.e('当前文本非JSON格式!')
-            L.e(e)
-        }
-    } else {
-        L.w('当前文本是空的')
+  let res = false
+  if (!isEmptyStr(str)) {
+    try {
+      let jsonObj = JSON.parse(str)
+      res = Array.isArray(jsonObj) && jsonObj.length > 0
+    } catch (e) {
+      L.e('当前文本非JSON格式!')
+      L.e(e)
     }
-    return res
+  } else {
+    L.w('当前文本是空的')
+  }
+  return res
 }
 
 /**
@@ -30,61 +30,73 @@ function fuCheck(str) {
  * @param str 待翻译文本
  * @param callback(res) 回调函数 res是json对象,-1表示翻译完成了
  * @param isCover 是否覆盖 true--覆盖翻译  false--如果之前已经翻译了，就跳过
+ * @param isCoverIfChsEqEng 是否在存在Chs节点时，且发现和Eng一样时执行覆盖操作
  */
-function fuTranslate(str, callback, isCover = false) {
-    const map = {}
-    const map_cn = {}
-    if (fuCheck(str)) {
-        let jsonObj = JSON.parse(str)
-        jsonObj.forEach((json, index) => {
-            if (json['Texts'] && json['Texts']['Eng']) {
-                map[index] = json['Texts']['Eng']
-            }
-            if (json['Texts'] && json['Texts']['Chs']) {
-                map_cn[index] = json['Texts']['Chs']
-            }
-        })
-    }
-    if (isCover) {
-        doTranslate(map, callback)
-    } else {
-        if (Object.keys(map_cn).length === Object.keys(map).length) {
-            if (typeof callback === 'function') {
-                callback(-1)
-            }
-        } else {
-            doTranslate(map, callback)
+function fuTranslate(str, callback, isCover = false, isCoverIfChsEqEng) {
+  const map = {}
+  const map_cn = {}
+  let jsonObj={}
+  if (fuCheck(str)) {
+    jsonObj = JSON.parse(str)
+    jsonObj.forEach((json, index) => {
+      if (json['Texts'] && json['Texts']['Eng']) {
+        map[index] = json['Texts']['Eng']
+      }
+      if (json['Texts'] && json['Texts']['Chs']) {
+        map_cn[index] = json['Texts']['Chs']
+      }
+    })
+  }
+  if (isCover) {
+    doTranslate(map, callback)
+  } else {
+    if (Object.keys(map_cn).length === Object.keys(map).length) {
+      if (isCoverIfChsEqEng) {
+        if(jsonObj.every(idx => map[idx] === map_cn[idx])){
+          doTranslate(map, callback)
+        }else{
+          if (typeof callback === 'function') {
+            callback(-1)
+          }
         }
+      } else {
+        if (typeof callback === 'function') {
+          callback(-1)
+        }
+      }
+    } else {
+      doTranslate(map, callback)
     }
+  }
 }
 
 function doTranslate(map, callback) {
-    L.i('发现待翻译文本:')
-    L.i(JSON.stringify(map,null,2))
-    translate(convertQryMap(map), ({trans_result}) => {
-        if (typeof callback === 'function') {
-            callback(trans_result)
-        }
-    })
+  L.i('发现待翻译文本:')
+  L.i(JSON.stringify(map, null, 2))
+  translate(convertQryMap(map), ({ trans_result }) => {
+    if (typeof callback === 'function') {
+      callback(trans_result)
+    }
+  })
 }
 
 /**
  * 将参数转换为百度翻译api能接受的参数
  * @param map
  * @returns {string} 由于百度api是要求所有个体参数以\n拼接，但是如果个体参数中存在\n，就会导致翻译返回的数组长度错位，进而会让重组失败
- * 因此，这里的做法就是在拼接时将个体参数中的\n转换为一个翻译保留字符，比如<>，翻译完后，再将<>还原为\n
+ * 因此，这里的做法就是在拼接时将个体参数中的\n转换为一个翻译保留字符，比如CJS翻译，翻译完后，再将CJS翻译还原为\n
  */
 function convertQryMap(map) {
-    let res = ''
-    Object.keys(map).forEach((key, index, keys) => {
-        res += map[key].replace(/\n/g, '<>')//替换个体参数里面的\n
-        if (index !== keys.length - 1) {
-            res += '\n'
-        }
-    })
-    L.d("请求参数合成结果:")
-    L.d(res)
-    return res;
+  let res = ''
+  Object.keys(map).forEach((key, index, keys) => {
+    res += map[key].replace(/\n/g, 'CJS翻译')//替换个体参数里面的\n
+    if (index !== keys.length - 1) {
+      res += '\n'
+    }
+  })
+  L.d("请求参数合成结果:")
+  L.d(res)
+  return res;
 }
 
 /**
@@ -92,27 +104,33 @@ function convertQryMap(map) {
  * @param srcObj
  * @param resObj
  * @param isCover 是否覆盖 true--覆盖翻译  false--如果之前已经翻译了，就跳过
+ * @param isCoverIfChsEqEng 是否在存在Chs节点时，且发现和Eng一样时执行覆盖操作
  */
-function fuRegroup(srcObj, resObj, isCover = false) {
-    if(!resObj){
-        L.e('翻译异常了，休息一会儿重来！')
-    }
-    srcObj.forEach((src, i) => {
-        //类比时，要考虑还原src和dst中的<>为\n
-        if (src['Texts'] && resObj[i] && resObj[i]['src'] && src['Texts']['Eng'] === resObj[i]['src'].replace(/<>/g, '\n')) {
-            const resDst = resObj[i]['dst'].replace(/<>/g, '\n')
-            if (isCover === true) {
-                src['Texts']['Chs'] = resDst
-            } else {
-                if (!src['Texts']['Chs']) {
-                    src['Texts']['Chs'] = resDst
-                }
-            }
+function fuRegroup(srcObj, resObj, isCover = false, isCoverIfChsEqEng) {
+  if (!resObj) {
+    L.e('翻译异常了，休息一会儿重来！')
+  }
+  srcObj.forEach((src, i) => {
+    //类比时，要考虑还原src和dst中的<>为\n
+    if (src['Texts'] && resObj[i] && resObj[i]['src'] && src['Texts']['Eng'] === resObj[i]['src'].replace(/CJS翻译/g, '\n')) {
+      const resDst = resObj[i]['dst'].replace(/CJS翻译/g, '\n')
+      if (isCover === true) {
+        src['Texts']['Chs'] = resDst
+      } else {
+        if (!src['Texts']['Chs']) {
+          src['Texts']['Chs'] = resDst
+        } else {
+          if (isCoverIfChsEqEng && src['Texts']['Chs'] === src['Texts']['Eng']) {
+            L.d('发现Eng等于Chs')
+            src['Texts']['Chs'] = resDst
+          }
         }
-    })
+      }
+    }
+  })
 }
 
 module.exports = {
-    fuTranslate,
-    fuRegroup
+  fuTranslate,
+  fuRegroup
 }
